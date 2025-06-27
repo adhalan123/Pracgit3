7f30bf00167aaab60c147c6439f1db6e0e355df8
adhalan123 <dhalangeaarati@gmail.com>
2025-06-27 21:53:51 +0530

    Even number Program

---
+++ src/test/java/Day1/Loop1.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class Loop1 {
+
+    //find even number
+
+    static int a=10;
+
+    public static void main(String[]args){
+
+        if((a % 2) == 0){
+
+            System.out.println("number is even number ");
+
+        }
+
+
+    }
+}
