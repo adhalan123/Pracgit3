0c9e78e26eda97dfd81d41d9521e231a96f92918
adhalan123 <dhalangeaarati@gmail.com>
2025-06-30 11:14:00 +0530

    Rebase chnage 2 RE2

--- src/test/java/Day1/StsGit.java
+++ src/test/java/Day1/StsGit.java
@@ -5,5 +5,7 @@ public class StsGit {
     public static void main (String[]args){
 
         System.out.println("this is for git practice and some revision");
+        System.out.println("This is rebase change 2nd Re2");
     }
 }
+
