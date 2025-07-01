66af09bf46f76ca5e66decbe88e03c461d30638b
adhalan123 <dhalangeaarati@gmail.com>
2025-07-01 21:31:40 +0530

    reverse string 1 program

---
+++ src/test/java/Day2/NewP.java
@@ -0,0 +1,21 @@
+package Day2;
+
+public class NewP {
+
+
+
+    public static void main(String[]args){
+
+        String a= "Aarati";
+
+        char[] b=a.toCharArray();
+
+        for(int i=b.length-1;i>=0;i--){
+
+            char n=b[i];
+
+            System.out.print(n);
+
+        }
+    }
+}
