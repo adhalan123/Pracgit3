34eee779fd7f737e15ec4be1b958a993b41e50a3
adhalan123 <dhalangeaarati@gmail.com>
2025-07-08 17:14:40 +0530

    some program and chnages

---
+++ src/test/java/Day4/sa2.java
@@ -0,0 +1,19 @@
+package Day4;
+
+public class sa2 {
+
+    public static void main(String[]args){
+
+        String a="aarati";
+
+        String b="";
+
+        for(int i=a.length()-1;i>=0;i--){
+
+           b=b+a.charAt(i);
+
+        }
+
+        System.out.println(b);
+    }
+}
